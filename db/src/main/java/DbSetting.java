2036e0c618c4167306ea87c71b8333f962486d2d
Nastena <freziay@mail.ru>
2023-02-01 14:58:43 +0300

    mavenProdgectnew

---
+++ db/src/main/java/DbSetting.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+
+public class DbSetting {
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
